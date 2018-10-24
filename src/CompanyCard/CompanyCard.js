3ceb33332248b0680f144c7321628b52c3d4a0d1
Sherry Li <shli@zendesk.com>
2018-10-25 10:37:10 +1100

    lists and conditionals study and add few styling study

--- src/CompanyCard/CompanyCard.js
+++ src/CompanyCard/CompanyCard.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import './CompanyCard.css'
+import React from "react";
+import "./CompanyCard.css";
 
-const companyCard = (props) => {
+const companyCard = props => {
   return (
-  <div className="CompanyCard">
-    <p>{props.name} has {props.people} people.</p>
-    {/* <p onClick={props.click}>{props.name} has {props.people} people.</p> */}
-    <p>{props.children}</p>
-    <input
-    onChange={props.nameChange}
-    />
-  </div>
-  // <div>
-  //   <p>Zusdesk has 1000 people</p>
-  //   {/* Math */}
-  //   </div>
-    
-)
-}
+    <div className="CompanyCard">
+      {/* <p>{props.name} has {props.people} people.</p> */}
+      <p onClick={props.click}>
+        {props.name} has {props.people} people.
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+    // <div>
+    //   <p>Zusdesk has 1000 people</p>
+    //   {/* Math */}
+    //   </div>
+  );
+};
 export default companyCard;
