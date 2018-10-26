711e08d66d7b02950d107cdff607054a6d7b0f0b
Sherry Li <shli@zendesk.com>
2018-10-26 12:48:05 +1100

    import css module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CompanyCard from "./CompanyCard/CompanyCard";
-import "./App.css";
+import cssClasses from "./App.css";
 // import Radium, { StyleRoot } from "radium";
 // import { white } from "ansi-colors";
 
@@ -104,14 +104,14 @@ class App extends Component {
     // let classes = ["red", "bold"].join(" ");
     const classes = [];
     if (this.state.companis.length <= 2) {
-      classes.push("red");
+      classes.push(cssClasses.red);
     }
     if (this.state.companis.length <= 1) {
-      classes.push("bold");
+      classes.push(cssClasses.bold);
     }
 
     return (
-      <div className="App">
+      <div className={cssClasses.App}>
         <header className="App-header">
           <h1>This is React intro 2.</h1>
           <p className={classes.join(" ")}>
