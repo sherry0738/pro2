466744b3f9f8d9440d481805daee1f8a63f210da
Sherry Li <shli@zendesk.com>
2018-11-07 16:04:41 +1100

    skip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+// import CompanyCard from "./CompanyCard/CompanyCard";
 
 class App extends Component {
   render() {
+    // return React.createElement("div", "h1", "this is React intro 2");
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>This is React intro 2.</h1>
+          <p>And it is step 2 for React study</p>
         </header>
       </div>
     );
